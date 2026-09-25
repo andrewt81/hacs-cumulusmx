ca08d346be95f41d8fbd269e9c42f83477b0e6a1
Andrea Tosatto <tosatto.andrea@gmail.com>
2026-09-25 17:12:39 +0200

    Add Cumulus MX Home Assistant integration

---
+++ custom_components/cumulusmx/__init__.py
@@ -0,0 +1,20 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from .const import DOMAIN
+from .coordinator import CumulusCoordinator
+
+PLATFORMS = ["sensor"]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    coordinator = CumulusCoordinator(hass, entry, async_get_clientsession(hass))
+    await coordinator.async_config_entry_first_refresh()
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    if await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+        return True
+    return False
