e5266a98c9bad34237d5fa262841364a1fbca569
Andrea Tosatto <tosatto.andrea@gmail.com>
2026-09-25 17:12:30 +0200

    Add Cumulus MX Home Assistant integration

---
+++ custom_components/cumulusmx/const.py
@@ -0,0 +1,4 @@
+DOMAIN = "cumulusmx"
+DEFAULT_PORT = 8998
+DEFAULT_INTERVAL = 30
+TAGS = ("version", "build", "temp", "hum", "dew", "press", "wspeed", "wgust", "bearing", "rfall", "rrate", "tempunitnodeg", "pressunit", "windunit", "rainunit")
